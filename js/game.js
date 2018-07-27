eca101cff47339430bc0ec39a5e7c66a99c298de
wangjiayong <you@example.com>
2018-07-27 12:33:27 +0800

    haha

--- js/game.js
+++ js/game.js
@@ -94,7 +94,7 @@
     }
     Game.prototype.gameOver = function () {
         this.running=false
-        alert('张丽羽你把小鸟玩死了')
+        alert('张丽羽你把小鸟玩死了，哈哈')
         location.reload()
     }
     fb.Game = Game
